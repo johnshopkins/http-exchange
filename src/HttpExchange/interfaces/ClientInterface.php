524fb978512de16640ba9be5638316ca2a0d1957
Jen Wachter <jwachter@jhu.edu>
2013-02-06 12:17:50 -0500

    Removed constructor from client interface.

--- src/HttpExchange/interfaces/ClientInterface.php
+++ src/HttpExchange/interfaces/ClientInterface.php
@@ -4,7 +4,6 @@ namespace HttpExchange\Interfaces;
 
 interface ClientInterface
 {
-	public function __construct($httpEngine);
 	public function get($url, $params, $headers);
 	public function getBody();
 	public function getStatusCode();
